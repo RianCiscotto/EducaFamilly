e55c87bb8690d41a6d5399a80a61451250c816af
Rian Ciscotto <Rianciscotto49@gmail.com>
2026-09-17 14:00:42 -0300

    Adiciona dominio do Render nas origens confiaveis do CSRF

--- EducaFamilly/settings.py
+++ EducaFamilly/settings.py
@@ -74,6 +74,11 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://educafamilly-chats.onrender.com',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
 
 # ============================================================
 # URLS / TEMPLATES
